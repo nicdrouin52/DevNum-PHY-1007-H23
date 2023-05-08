f8367fbdc7e68656997731c489138b2d122ad56c
Cassandra Mantha <caman29@ulaval.ca>
2023-05-08 13:31:44 -0400

    circuit_c_début

--- examples/Circuit_c.py
+++ examples/Circuit_c.py
@@ -21,32 +21,16 @@ if __name__ == "__main__":
     horizontal_eqs = (x_expression_horizontal, y_expression_horizontal)
 
     wires = [
-        Wire((20, 20), (20, 48), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        VoltageSource((20, 48), (20, 52), vertical_eqs, cartesian_variables, BATTERY_VOLTAGE),
-        Wire((20, 52), (20, 80), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-
-        Wire((20, 80), (80, 80), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-
-        Wire((80, 80), (80, 52), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        VoltageSource((80, 52), (80, 48), vertical_eqs, cartesian_variables, BATTERY_VOLTAGE),
-        Wire((80, 48), (80, 20), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-
-        Wire((80, 20), (20, 20), horizontal_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-
-        Wire((40, 20), (40, 45), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((40, 45), (40, 55), vertical_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE),
-        Wire((40, 55), (40, 80), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-
-        Wire((60, 20), (60, 45), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE),
-        Wire((60, 45), (60, 55), vertical_eqs, cartesian_variables, HIGH_WIRE_RESISTANCE),
-        Wire((60, 55), (60, 80), vertical_eqs, cartesian_variables, LOW_WIRE_RESISTANCE)
+    
     ]
     ground_position = (80, 48)
+
 
     circuit = Circuit(wires, ground_position)
     world = World(circuit=circuit, coordinate_system=CoordinateSystem.CARTESIAN, shape=WORLD_SHAPE)
     world.show_circuit(
-        {0: (48, 80), 1: (80, 20), 2: (60, 20), 3: (40, 20), 4: (20, 20), 5: (20, 48), 6: (20, 80), 7: (40, 80), 8: (60, 80), 9: (80, 80)}
+        {0: (80, 48), 1:(80, 20), 2:(60, 20), 3: (40, 20), 4: (20, 20), 5: (20, 48), 6: (20, 52), 7: (20, 80),
+         8: (40, 80), 9: (60, 80), 10: (80, 80), 11: (80, 52), 12: (40, 55), 13: (40, 45), 14: (60, 55), 15: (60, 45)}
     )
     world.compute()
     world.show_potential() # à la fin, on va avoir show_all. Je l'ai remplacé temporairement
