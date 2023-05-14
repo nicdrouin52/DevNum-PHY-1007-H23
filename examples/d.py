33a24be663b10fd7a4b56af2f8e87ab76131c4c0
David Vincent <davin23@ulaval.ca>
2023-05-13 21:26:06 -0400

    Fini?

---
+++ examples/d.py
@@ -0,0 +1,43 @@
+import env_examples  # Modifies path, DO NOT REMOVE
+from sympy import Symbol, cos, sin
+import numpy as np
+from scipy.constants import pi
+from src import Circuit, CoordinateSystem, VoltageSource, Wire, World
+
+
+if __name__ == "__main__":
+    WORLD_SHAPE = (101, 101)
+    BATTERY_VOLTAGE = 1.0
+    HIGH_WIRE_RESISTANCE = 1.0
+    LOW_WIRE_RESISTANCE = 0.01
+
+    polaire_variables = Symbol("r"), Symbol("theta")
+    r, theta = polaire_variables
+
+    
+    radial_eqs = (r, 0*theta)
+    #for angle in range(45, 76, 1):
+    #    x, y = r*cos(radians(angle)), r*sin(radians(angle))
+    angular_eqs = r*np.(theta), r*np.sin(theta)
+
+
+    wires = [
+        Wire((50, pi/4), (50, 19*pi/45), angular_eqs, polaire_variables, LOW_WIRE_RESISTANCE),
+        Wire((50, 19*pi/45), (70, 19*pi/45), radial_eqs, polaire_variables, LOW_WIRE_RESISTANCE),
+        Wire((70, 19*pi/45), (70, 5*pi/18), angular_eqs, polaire_variables, LOW_WIRE_RESISTANCE),
+        Wire((70, 5*pi/18), (70, 13*pi/60), angular_eqs, polaire_variables, HIGH_WIRE_RESISTANCE),
+        Wire((70, 13*pi/60), (70, 7*pi/90), angular_eqs, polaire_variables, LOW_WIRE_RESISTANCE),
+        Wire((70, 7*pi/90), (50, 7*pi/90), radial_eqs, polaire_variables, LOW_WIRE_RESISTANCE),
+        Wire((50, 7*pi/90), (50, 43*pi/180), angular_eqs, polaire_variables, LOW_WIRE_RESISTANCE),
+        VoltageSource((50, 43*pi/180), (50, pi/4), angular_eqs, polaire_variables, BATTERY_VOLTAGE),
+    ]
+    ground_position = (50, 43*pi/180)
+
+
+    circuit = Circuit(wires, ground_position)
+    world = World(circuit=circuit, coordinate_system=CoordinateSystem.POLAR, shape=WORLD_SHAPE)
+    world.show_circuit(
+        {0: (50, pi/4), 1:(50, 19*pi/45), 2:(70, 19*pi/45), 3: (70, 5*pi/18), 4: (70, 13*pi/60), 5: (70, 7*pi/90), 6: (50, 7*pi/90), 7: (50, 43*pi/180)}
+    )
+    world.compute()
+    world.show_potential()
--- src/biot_savart_equation_solver.py
+++ src/biot_savart_equation_solver.py
@@ -134,56 +134,70 @@ class BiotSavartEquationSolver:
         """
         # Si le champs du courant est en 2D on le transforme en
         # un champs 3D à coordonnées I_z = 0
-        if electric_current.shape[2] == 2:
-            B = np.zeros(shape=(electric_current.shape[0], electric_current.shape[1], 3))
-            B[:,:, :2] = electric_current
-            B = VectorField(B)
-        else:
-            B = electric_current.copy()
+#        if electric_current.shape[2] == 2:
+#            B = np.zeros(shape=(electric_current.shape[0], electric_current.shape[1], 3))
+#            B[:,:, :2] = electric_current
+#            B = VectorField(B)
+#        else:
+#            B = electric_current.copy()
         # juste un copier/coller de cartesien...
 
         # on crée une liste vide pour les points où circule un courant
-        Courant = []
+#        Courant = []
         # on ajoute tous les points où circule un courant
-        for indice_rayon, rayon in enumerate(electric_current):
-            for indice_theta, theta in enumerate(rayon):
-                if len(theta) == 3:
-                    if theta[0] or theta[1] or theta[2] != 0:
-                        Courant.append([indice_rayon, indice_theta])
-                else:
-                    if theta[0] or theta[1] != 0:
-                        Courant.append([indice_rayon, indice_theta])
+#        for indice_rayon, rayon in enumerate(electric_current):
+#            for indice_theta, theta in enumerate(rayon):
+#                if len(theta) == 3:
+#                    if theta[0] or theta[1] or theta[2] != 0:
+#                        Courant.append([indice_rayon, indice_theta])
+#                else:
+#                    if theta[0] or theta[1] != 0:
+#                        Courant.append([indice_rayon, indice_theta])
 
         # on itère sur toute la grille
-        for indice_rayon, rayon in enumerate(B):
-            for indice_theta, theta in enumerate(rayon):
+#        for indice_rayon, rayon in enumerate(B):
+#            for indice_theta, theta in enumerate(rayon):
                 # on exclut les points où circule un courant
-                if [indice_rayon, indice_theta] not in Courant:
+#                if [indice_rayon, indice_theta] not in Courant:
                     # on initialise le résultat de l'intégrale
                     # issu de la loi de Biot-Savart
-                    integrale = [0,0,0]
+#                    integrale = [0,0,0]
                     # on somme la contribution au champs de chaque 
                     # petit bout de fil où circule un courant
-                    for i in Courant:
+#                    for i in Courant:
                         # vecteur de l'origine -> point où le champs est calculé
-                        r = [indice_rayon*delta_r, indice_theta*delta_theta]
+#                        r = [indice_rayon*delta_r, indice_theta*delta_theta]
                         # vecteur de l'origine -> élément de fil
-                        r_prime = [i[0]*delta_r, i[1]*delta_theta]
+#                        r_prime = [i[0]*delta_r, i[1]*delta_theta]
                         # vecteur de l'élément de fil -> point où le champs est calculé
-                        r_cursif = np.subtract(r, r_prime)
+#                        r_cursif = np.subtract(r, r_prime)
 
                         # norme de r cursif
-                        norme_r_cursif = np.linalg.norm(r_cursif)
+#                        norme_r_cursif = np.linalg.norm(r_cursif)
                         # courant à du au bout de fil
-                        I = B[i[0]][i[1]]
+#                        I = B[i[0]][i[1]]
                         # on ajoute la contribution de ce petit bout de fil à notre intégrale
-                        integrale += np.cross(I,r_cursif)/norme_r_cursif**2
+#                        integrale += np.cross(I,r_cursif)/norme_r_cursif**2
                     
                     # de la loi de Biot-Savart :
-                    B[indice_rayon][indice_theta] = mu_0*integrale/(4*pi)
+#                    B[indice_rayon][indice_theta] = mu_0*integrale/(4*pi)
 
 
-        return B
+#        return B
+        Coordonné_circuit = np.array([(r, theta) for r, row in enumerate(electric_current) for theta, val in enumerate(row) if val.any()])
+
+        champ_B = np.zeros((electric_current.shape[0], electric_current.shape[1], 3))
+        for i in range(electric_current.shape[0]):
+            for j in range(electric_current.shape[1]):
+                r = np.stack((Coordonné_circuit[:, 0] - i, Coordonné_circuit[:, 1] - j, np.zeros(len(Coordonné_circuit[:, 0]))), axis=-1)
+
+                module_r = np.sqrt((r ** 2).sum(axis=-1))
+
+                B = np.cross(r, electric_current[Coordonné_circuit[:, 0], Coordonné_circuit[:, 1]])[:, 2] / (module_r ** 3)
+                champ_B[i, j][2] = np.sum(B, axis=0)
+    
+        return VectorField(np.nan_to_num(mu_0 * champ_B / (4 * np.pi), nan=0))
+
 
     def solve(
             self,
